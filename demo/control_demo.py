d5fd428d439ac413bb17776842eb7f6069becf04
Magnus Theodor Engh <dor@ssb.no>
2024-10-01 09:11:47 +0200

    Legger til rammeverk for kontroller og kontrollmodal

---
+++ demo/control_demo.py
@@ -0,0 +1,76 @@
+# %%
+import numpy as np
+import pandas as pd
+from kontroll_rammeverk import Kontrolltype
+from kontroll_rammeverk import automatisk_oppretting
+from kontroll_rammeverk import kontroll
+from kontroll_rammeverk import lag_kvalitetsrapport
+
+eksempel_data = pd.DataFrame(
+    {"Alder": [120, 90, -50, 10, 20, 40], "ident": [1, 2, 3, 4, 5, 6]}
+)
+
+
+# %%
+@kontroll(
+    id_column="ident",
+    result_type=Kontrolltype.MULIG_FEIL,
+    error_description="Veldig høy alder",
+)
+def min_kontrollfunksjon_1(data):
+    """Sjekker etter ekstremt høy alder"""
+    data["utslag"] = False
+    betingelse = data["Alder"] > 100
+    data.loc[betingelse, "utslag"] = True
+    return data
+
+
+@kontroll(
+    id_column="ident",
+    result_type=Kontrolltype.ABSOLUTT_FEIL,
+    error_description="Ugyldig verdi",
+)
+def min_kontrollfunksjon_2(data):
+    """Sjekker at alder har gyldig verdi"""
+    data["utslag"] = False
+    betingelse = data["Alder"] < 0
+    data.loc[betingelse, "utslag"] = True
+    data = min_kontrollfunksjon_2_automatisk_retting(data)
+    return data
+
+
+@automatisk_oppretting(
+    id_column="ident", correction_description="Erstatter ugyldig alder med nan"
+)
+def min_kontrollfunksjon_2_automatisk_retting(data):
+    data["maskinelt_rettet"] = False
+    betingelse = data["utslag"] == True
+    data.loc[betingelse, "Alder"] = np.nan
+    data.loc[betingelse, "maskinelt_rettet"] = True
+    return data
+
+
+# %%
+min_kontrollfunksjon_1(eksempel_data[eksempel_data["Alder"].notna()])
+min_kontrollfunksjon_2(eksempel_data)
+
+# %%
+feilrapport, kontrollrapport = lag_kvalitetsrapport(
+    statistics_name="Demo",
+    data_location="path/to/data",
+    data_period="2024K1",
+)
+
+# %%
+feilrapport.to_dict()
+
+# %%
+pd.DataFrame().from_dict(feilrapport.to_dict()["kontrollutslag"])
+
+# %%
+pd.DataFrame().from_dict(feilrapport.to_dict()["kontrolldokumentasjon"], orient="index")
+
+# %%
+kontrollrapport
+
+# %%
