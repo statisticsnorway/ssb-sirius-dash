444bd09d163f5ec85305793b0d75c3b0675fccdc
ssb-dor <dor@ssb.no>
2025-02-28 07:49:09 +0000

    Kind of temporary fix to R package problem

--- src/ssb_sirius_dash/__init__.py
+++ src/ssb_sirius_dash/__init__.py
@@ -4,8 +4,6 @@ from . import control
 from . import modals
 from . import setup
 from . import tabs
-from .kostra_r_wrapper import _get_kostra_r
-from .kostra_r_wrapper import hb_method
 from .kostra_r_wrapper import th_error
 
 # Re-import functions and classes from submodules explicitly for top-level access
