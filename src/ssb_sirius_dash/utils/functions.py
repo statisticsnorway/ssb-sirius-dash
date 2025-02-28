90cc670a0c17e5928f865a09fa2e6b6ff35cfe72
ssb-dor <dor@ssb.no>
2025-02-28 08:05:35 +0000

    Remove old code and move th_error

--- src/ssb_sirius_dash/utils/functions.py
+++ src/ssb_sirius_dash/utils/functions.py
@@ -78,6 +78,24 @@ def hb_method(
         )
 
 
+def th_error(
+    data: pd.DataFrame, id_field_name: str, x_1_field_name: str, x_2_field_name: str
+) -> pd.DataFrame:
+    """Runs the ThError method from the R package Kostra.
+
+    :param data: The data to run the method on
+    :param id_field_name: The name of the id field
+    :param x_1_field_name: The name of the first x field
+    :param x_2_field_name: The name of the second x field
+    :return: The result of the method
+    """
+    with conversion.localconverter(default_converter + pandas2ri.converter):
+        th_error_result = _get_kostra_r().ThError(
+            data=data, id=id_field_name, x1=x_1_field_name, x2=x_2_field_name
+        )
+        return th_error_result[th_error_result.outlier == 0]
+
+
 def format_timespan(start: int | float, end: int | float) -> str:
     """Formats the elapsed time between two time points into a human-readable string.
 
