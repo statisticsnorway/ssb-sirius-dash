4e9b3bc25c1a3dd7e22709bfab925125f063278b
ssb-dor <dor@ssb.no>
2025-03-14 07:44:06 +0000

    Debugger modal for helping user understand how dynamic_states and args interact

--- src/ssb_sirius_dash/utils/debugger_modal.py
+++ src/ssb_sirius_dash/utils/debugger_modal.py
@@ -1,20 +1,26 @@
-from ..setup.variableselector import VariableSelector
-from ..utils.functions import sidebar_button
+import dash_bootstrap_components as dbc
 from dash import Input
 from dash import Output
 from dash import State
 from dash import callback
-
-from dash import html
-import dash_bootstrap_components as dbc
 from dash import callback_context
+from dash import html
+
+from ..setup.variableselector import VariableSelector
+from ..utils.functions import sidebar_button
+
 
 class DebugInspector:
 
-    def __init__(self, inputs, states):
+    def default_func(*args):
+        return html.Plaintext(f"{args!s}")
+
+    def __init__(self, inputs, states, func=default_func):
+        self.func = func
+        self.inputs = inputs
+        self.states = states
         self.variableselector = VariableSelector(inputs, states)
         self.callbacks()
-        
 
     def layout(self):
         return html.Div(
@@ -23,23 +29,30 @@ class DebugInspector:
                     [
                         dbc.ModalHeader(dbc.ModalTitle("DebugInspector")),
                         dbc.ModalBody(
-                            [dbc.Row(id="debuggerhelper_output")]
-                        )
+                            [
+                                dbc.Row(id="debuggerhelper_output"),
+                                dbc.Row(
+                                    "Below is the output from the function passed to the debugger"
+                                ),
+                                dbc.Row(html.Div(id="debuggerhelper_func_output")),
+                            ]
+                        ),
                     ],
-                    id = "debugger_modal",
+                    id="debugger_modal",
                     size="xl",
-                    fullscreen = "xxl-down"
+                    fullscreen="xxl-down",
                 ),
                 sidebar_button("🥼", "Debugger", "sidebar-debugger-button"),
             ]
         )
 
     def callbacks(self):
-        
+
         dynamic_states = [
             self.variableselector.get_inputs(),
             self.variableselector.get_states(),
         ]
+
         @callback(  # type: ignore[misc]
             Output("debugger_modal", "is_open"),
             Input("sidebar-debugger-button", "n_clicks"),
@@ -49,13 +62,32 @@ class DebugInspector:
             if n:
                 return not is_open
             return is_open
-        
+
         @callback(Output("debuggerhelper_output", "children"), *dynamic_states)
         def debuggerhelper_dynamic_states(*args):
             ctx = callback_context  # Get callback context
-        
+
             if not ctx.triggered:
                 return "No input yet."
 
-            return html.Div([dbc.Row([html.Plaintext(f"state: {str(x)}") for x in args]), dbc.Row(html.Plaintext(f"{ctx.inputs}, {ctx.states}"))])
+            return html.Div(
+                [
+                    dbc.Row(html.Plaintext("Arguments sent into the class")),
+                    dbc.Row(html.Plaintext(f"Inputs: {self.inputs}")),
+                    dbc.Row(html.Plaintext(f"States: {self.states}")),
+                    dbc.Row(
+                        html.Plaintext(
+                            "Information about the callback Inputs and States."
+                        )
+                    ),
+                    dbc.Row(html.Plaintext(f"Inputs: {ctx.inputs}")),
+                    dbc.Row(html.Plaintext(f"States: {ctx.states}")),
+                    dbc.Row(html.Plaintext("Args from *dynamic_states:")),
+                    dbc.Row(html.Plaintext(f"{args}")),
+                    dbc.Row([html.Plaintext(f"{x!s}, {type(x)}") for x in args]),
+                ]
+            )
 
+        @callback(Output("debuggerhelper_func_output", "children"), *dynamic_states)
+        def debuggerhelper_func(*args):
+            return self.func(args)
